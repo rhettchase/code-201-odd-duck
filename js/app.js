a95a0c93d63d647e0c6ff2c8efa2065218470efb
RHETT CHASE <rhettjchase@gmail.com>
2023-11-07 14:07:05 -0800

    bar chart rendering as expected

--- js/app.js
+++ js/app.js
@@ -8,22 +8,39 @@ const ulElem = document.querySelector('ul');
 const sectionElem = document.getElementById('resultsHeader');
 const button = document.getElementById('showResults');
 
-
 // querySelectorAll = list of elements
 // querySelector = 1 element
 // could also use document.getElementById
 const leftProductImage = document.querySelector('section img:first-child');
 const middleProductImage = document.querySelector('section img:nth-child(2)');
 const rightProductImage = document.querySelector('section img:nth-child(3)');
-const allProductNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
+const allProductNames = [
+  'bag',
+  'banana',
+  'bathroom',
+  'boots',
+  'breakfast',
+  'bubblegum',
+  'chair',
+  'cthulhu',
+  'dog-duck',
+  'dragon',
+  'pen',
+  'pet-sweep',
+  'scissors',
+  'shark',
+  'sweep',
+  'tauntaun',
+  'unicorn',
+  'water-can',
+  'wine-glass',
+];
 
 let leftProductInstance = null;
 let middleProductInstance = null;
 let rightProductInstance = null;
 let clickCount = 0; // running total of votes
-const maxClicks = 25; // max number of votes
-
-
+const maxClicks = 10; // max number of votes
 
 function Product(name, src) {
   this.name = name;
@@ -44,7 +61,6 @@ function initProducts() {
     Product.allProducts.push(productInstance);
   }
 }
-
 
 function renderProducts() {
   // check if click count has reached max
@@ -76,10 +92,65 @@ function renderProducts() {
   rightProductInstance.views += 1;
 }
 
+function renderChart() {
+  let productNames = [];
+  let productVotes = [];
+  let productViews = [];
+
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    productNames.push(Product.allProducts[i].name);
+    productVotes.push(Product.allProducts[i].clicks);
+    productViews.push(Product.allProducts[i].views);
+  }
+  /* refer to Chart.js > Chart Types > Bar Chart:
+  https://www.chartjs.org/docs/latest/charts/bar.html
+  and refer to Chart.js > Getting Started > Getting Started:
+  https://www.chartjs.org/docs/latest/getting-started/ */
+  const data = {
+    labels: productNames,
+    datasets: [{
+      label: 'Votes',
+      data: productVotes,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)'
+      ],
+      borderWidth: 1
+    },
+    {
+      label: 'Views',
+      data: productViews,
+      backgroundColor: [
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 159, 64)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+  let canvasChart = document.getElementById('myChart');
+  const myChart = new Chart(canvasChart, config);
+}
+
+
+
 ///////////////////////////////////
 // HELPER FUNCTIONS  ///////////////
-
-
 
 //randomize array
 // Fisher Yates function via chatGPT
@@ -109,17 +180,20 @@ function handleRightClick() {
   renderProducts();
 }
 
-
-function renderResultsClick() {
-  for (let i = 0; i < Product.allProducts.length; i++) {
-    let currentProduct = Product.allProducts[i];
-    let result = `${currentProduct.name} had ${currentProduct.clicks} votes and was viewed ${currentProduct.views} times.`;
-    // console.log(result);
-    const liElem = document.createElement('li');
-    ulElem.appendChild(liElem);
-    liElem.textContent = result;
-  }
+function handleViewResultsClick() {
+  renderChart();
 }
+
+// function renderResultsClick() {
+//   for (let i = 0; i < Product.allProducts.length; i++) {
+//     let currentProduct = Product.allProducts[i];
+//     let result = `${currentProduct.name} had ${currentProduct.clicks} votes and was viewed ${currentProduct.views} times.`;
+//     // console.log(result);
+//     const liElem = document.createElement('li');
+//     ulElem.appendChild(liElem);
+//     liElem.textContent = result;
+//   }
+// }
 
 // setup listeners in callback
 function setupListeners() {
@@ -136,7 +210,7 @@ function removeListener() {
 }
 
 function renderResultsButton() {
-  button.addEventListener('click', renderResultsClick);
+  button.addEventListener('click', handleViewResultsClick);
   button.style.display = 'block';
   button.removeAttribute('hidden');
   const resultsHeaderElem = document.createElement('h2');
@@ -150,43 +224,3 @@ function renderResultsButton() {
 initProducts();
 renderProducts();
 setupListeners();
-
-// let bag = new Product('bag', './img/bag.jpg');
-// let banana = new Product('banana', './img/banana.jpg');
-// let bathroom = new Product('bathroom', './img/bathroom.jpg');
-// let boots = new Product('boots', './img/boots.jpg');
-// let breakfast = new Product('breakfast', './img/breakfast.jpg');
-// let bubblegum = new Product('bubblegum', './img/bubblegum.jpg');
-// let chair = new Product('chair', './img/chair.jpg');
-// let cthulhu = new Product('cthulhu', './img/cthulhu.jpg');
-// let dogDuck = new Product('dog-duck', './img/dog-duck.jpg');
-// let dragon = new Product('dragon', './img/dragon.jpg');
-// let pen = new Product('pen', './img/pen.jpg');
-// let petSweep = new Product('pet-sweep', './img/pet-sweep.jpg');
-// let scissors = new Product('scissors', './img/scissors.jpg');
-// let shark = new Product('shark', './img/shark.jpg');
-// let sweep = new Product('sweep', './img/sweep.png');
-// let tauntaun = new Product('tauntaun', './img/tauntaun.jpg');
-// let unicorn = new Product('unicorn', './img/unicorn.jpg');
-// let waterCan = new Product('water-can', './img/water-can.jpg');
-// let wineGlass = new Product('wine-glass', './img/wine-glass.jpg');
-
-// allProducts.push(bag);
-// allProducts.push(banana);
-// allProducts.push(bathroom);
-// allProducts.push(boots);
-// allProducts.push(breakfast);
-// allProducts.push(bubblegum);
-// allProducts.push(chair);
-// allProducts.push(cthulhu);
-// allProducts.push(dogDuck);
-// allProducts.push(dragon);
-// allProducts.push(pen);
-// allProducts.push(petSweep);
-// allProducts.push(scissors);
-// allProducts.push(shark);
-// allProducts.push(sweep);
-// allProducts.push(tauntaun);
-// allProducts.push(unicorn);
-// allProducts.push(waterCan);
-// allProducts.push(wineGlass);
